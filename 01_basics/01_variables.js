8354e03c6022ba11201c2f9a47e9002d81354be3
Teerth Kolhi <97418435+TEERTH-KOLHI@users.noreply.github.com>
2024-07-16 13:11:55 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12232
+let accountName = "Project"
+var accountlife = "side"
+worldRecord = "apex"
+let projectState;
+
+console.table([accountId, accountName, accountlife, worldRecord, projectState ])
